dc4883483986203409ca69cf40f994a0d160367e
Harishkadimi <kadimiharish007@gmail.com>
2023-03-21 19:00:21 +0530

    correcting the tox code

--- tox_poetry_practice/tests/test_main.py
+++ tox_poetry_practice/tests/test_main.py
@@ -1,3 +1,23 @@
+import requests
 
-def test_check_endPoint(:
-    assert 'string' == 'ttring'
+ENDPOINT = "https://todo.pixegami.io/"
+response = requests.get(ENDPOINT)
+def test_check_endPoint():
+    response = requests.get(ENDPOINT)
+    assert response.status_code == 200
+
+def test_crete_item():
+    payload = {
+        "content": "string",
+        "user_id": "string",
+        "is_done": False
+        }
+    response = requests.put(ENDPOINT + "/create-task", json=payload)
+    assert response.status_code == 200
+    data = response.json()
+    task_id = data['task']['task_id']
+    get_task_response = requests.get(ENDPOINT + f'/get-task/{task_id}')
+    assert get_task_response.status_code == 200
+    get_task_data = get_task_response.json()
+    assert get_task_data['content'] == payload['content']
+    assert get_task_data['user_id'] == payload['user_id']
